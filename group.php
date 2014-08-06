963affcd93060f3f0d417c4213c1eb766cd2d4c6
James Lemieux <jrlmx2@gmail.com>
2014-08-06 09:06:58 -0700

    Slimmed interface to get rid of unnecssary declarations

--- group.php
+++ group.php
@@ -42,15 +42,6 @@ interface group
 	public static register_post_types();
 
 /*
- * Register group taxonomies
- *
- *
- * @return void
- */
-
-	public static register_tax_types();
-
-/*
  * Register valid group status for workflow
  * 
  *
@@ -58,55 +49,7 @@ interface group
  * @return void
  */
 
-	public static register_status_types();
-
-/*
- * Returns all current members of group
- *
- * @param int $group_id ID of the group to retrieve members
- *
- * @return array of group member ids
- */
 	public static get_group_members( $group_id );
-
-/*
- * Add a member to a group
- *
- * @param int $group_id ID of the group to retrieve members
- * @param int $member_id ID of the group to retrieve members
- *
- * @return bool was the addition successful
- */
-	public static add_group_member( $group_id, $member_id );
-
-/*
- * Remove a member to a group
- *
- * @param int $group_id ID of the group to retrieve members
- * @param int $member_id ID of the group to retrieve members
- *
- * @return bool was the removal successful
- */
-	public static remove_group_member( $group_id, $member_id );
-
-/*
- * List the most newly created groups limited on $limit. If limit is not set
- * the default value of the GroupPress option GroupPress_groups_per_page
- *
- * @param int $limit Upper limit of amount of groups retrieved
- *
- * @return mixed The value false is returned on failure. An array of group_ids is returned otherwise
- */
-	public static list_latest_groups( $limit );
-
-/*
- * The current active group information returned in an array based on wordpress post_table
- * TODO add an example response
- *
- *
- * @return mixed The value false is returned on failure. An array of group information is returned otherwise
- */
-	public group_to_array();
 
 /*
  * Return the current active group member IDs
@@ -134,50 +77,21 @@ interface group
  */
 	public add_group_meta( $meta_label, $meta_value );
 
-/*
- * Add member to current group
- *
- * @param int $member_id ID of the Member to add to the group. This action places the member in the "requested" status
- * @param bool $active Whether the intended member should go strait to active and bypass "requested" status. Default value is false. True bypass "requested" status.  
- *
- * @return bool false on failure, true on success
- */
-	public add_group_member( $member_id, $active=false );
-
-/*
- * Remove member to current group
- *
- * @param int $member_id ID of the Member to remove from the current group. This action removes a member. The members status in the database is marked as removed.
- *
- * @return bool false on failure, true on success
- */
-	public remove_group_member( $member_id );
-
-/*
- * List the members of the current active group in the "requested" status.
- *
- *
- * @return array The array of member IDs of pending members.
- */
-	public list_pending_members();
 
 /*
  * Mark a member as inactive in the group
  *
  * @param int $member_id ID of the Member to remove from the current group. This action removes a member. The members status in the database is marked as removed.
+ * @param string $status status of the member update. expected values are as follows:
+ *     "new" (default) => add member to the new members list
+ *     "active" => add member to the active list.
+ *     "inactive" => add member to the inactive list.
+ *     "remove" => remove member from group
+ *
+ *     *note In all situations, the member is first removed from all current lists, and then reassigned accordingly
  *
  * @return bool true on success, false on failure
  */
-	public mark_member_inactive( $member_id ));
-
-/*
- * Mark a member as active in the group
- *
- * @param int $member_id ID of the Member to remove from the current group. This action removes a member. The members status in the database is marked as removed.
- *
- * @return bool true on success, false on failure
- */
-	public activate_member( $member_id );
-
+	public update_group_member( $member_id, $status='new' );
 }
 ?>
